625d30e3751af2b994e147b131549c52ddf6071b
sundar <sundarkande445@gmail.com>
2024-07-13 12:11:07 +0530

    Count API test
    Created count api test

---
+++ src/test/java/com/api/test/CountAPITest.java
@@ -0,0 +1,11 @@
+package com.api.test;
+
+public class CountAPITest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("count api test");
+
+	}
+
+}
